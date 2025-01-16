b1170356841e0179dc4abacab76721f41e7f1773
Hosein <hosein.abolhasani1998@gmail.com>
2025-01-16 14:18:14 +0330

    fix:typo

--- yt-next/src/app/products/page.tsx
+++ yt-next/src/app/products/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <>
       <div className="mb-6">
         <h3 className="text-3xl font-bold">Products</h3>
-        <p className="text-slate-800 text-sm">lis of all products in stocks</p>
+        <p className="text-slate-800 text-sm">lis of all products in stock</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {products.map((product) => (
