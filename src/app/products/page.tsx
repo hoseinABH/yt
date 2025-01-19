157a22aca04f37f66139ffeaa26b2ef2d1e68c84
hosein <hosein.abolhasani1998@gmail.com>
2025-01-19 08:00:08 +0330

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -13,7 +13,7 @@ export default async function Products() {
       {/* Static Content */}
       <div className="mb-6">
         <h3 className="text-3xl font-bold">Products</h3>
-        <p className="text-slate-800 text-sm">lis of all products in stock</p>
+        <p className="text-slate-800 text-sm">list of all products in stock</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {/* Dynamic Content */}
